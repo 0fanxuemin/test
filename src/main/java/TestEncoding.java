c09c160507a0873e6d3b39e64a883d74cf86399f
fanxuemin <fanxuemin@lianlife.com>
2020-05-06 09:42:13 +0800

    test commit

---
+++ src/main/java/TestEncoding.java
@@ -0,0 +1,19 @@
+import java.io.UnsupportedEncodingException;
+
+public class TestEncoding {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String a="mifan米饭";
+        System.out.println(a.getBytes("unicode").toString());
+        System.out.println(new String(a.getBytes("unicode"),"unicode"));
+        String b=new String("mifan米饭")+"mifan";
+        if(a==b){
+            System.out.println("true");
+        }
+        String A="a1";
+        int B=1;
+        String C="a"+B;
+        System.out.println(A==C);
+        System.out.println(A.equals(C));
+    }
+}
+
